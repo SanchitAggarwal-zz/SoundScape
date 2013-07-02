83d64b6d84465d3942789dea7abc41ccaac13918
sanchitaggarwal <sanchitagarwal108@gmail.com>
2013-07-02 10:20:06 +0530

    added css

--- index.py
+++ index.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+from forms import InformationForm
 
 app = Flask(__name__)
-
+app.secret_key= 'experiment@2013'
 
 @app.route('/training')
 def training():
+    """
+
+
+    :return:
+    """
     return "Training"
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    form=InformationForm()
+    return render_template('home.html',form=form)
 
 @app.route('/testing')
 def testing():
